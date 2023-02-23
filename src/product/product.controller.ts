8d5d01fdd3f27c3e3a2693793ee31555fb7c5436
popov.ar <popov.art.a@gmail.com>
2023-02-23 18:27:40 +0400

    test: test commit

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,7 +9,7 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
-//import { CreateProductDto } from './dto/create-product.dto';
+// import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { FindProductDto } from './dto/find-product.dto';
 import { ProductModel } from './model/product.model';
